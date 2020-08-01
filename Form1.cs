b790517469a8126f9f27908eb54b3748c24ea858
YellowBlueBuzz <52501486+YellowBlueBuzz@users.noreply.github.com>
2020-08-01 10:23:22 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace Readme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string naniresult = client.GetStringAsync("https://example.com/test.txt").Result;
+            nani.Text = naniresult;
+
+        }
+
+
+
+
+    }
+}
